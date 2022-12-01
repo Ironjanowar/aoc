07e57fa650a59dfb289c8940b0c09114c971210e
Cesar Guzman <cesar.guzman@meetsam.io>
2022-12-01 21:47:45 +0100

    Try task

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -1,6 +1,11 @@
 defmodule AOC do
+  @callback parse(String.t()) :: any()
+  @callback solve(any()) :: any()
+
   defmacro __using__(_) do
     quote do
+      @behaviour AOC
+
       def test() do
         parse_and_solve("test_input")
       end
@@ -15,14 +20,6 @@ defmodule AOC do
 
       defp parse_input(file) do
         file |> File.read!() |> parse()
-      end
-
-      defp parse(text) do
-        text
-        |> String.split("\n\n")
-        |> Enum.map(fn pack ->
-          pack |> String.split("\n") |> Enum.map(&String.to_integer/1)
-        end)
       end
     end
   end
