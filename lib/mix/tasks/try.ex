ef7ecaf9621ac9b20e95dd0e3bd9dcc3ade58d34
Cesar Guzman <cesar.guzman@meetsam.io>
2022-12-01 21:45:02 +0100

    Try task

--- lib/mix/tasks/try.ex
+++ lib/mix/tasks/try.ex
@@ -2,7 +2,8 @@ defmodule Mix.Tasks.Try do
   use Mix.Task
 
   @impl Mix.Task
-  def run(_args) do
-    Mix.shell().info(Cuwano.test())
+  def run([module]) do
+    module = String.to_existing_atom(module)
+    Mix.shell().info(module.test())
   end
 end
