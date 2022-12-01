829bad9f730cf76785feb03391de32894b63339a
Cesar Guzman <cesar.guzman@meetsam.io>
2022-12-01 21:51:28 +0100

    Task

--- lib/mix/tasks/try.ex
+++ lib/mix/tasks/try.ex
@@ -2,8 +2,9 @@ defmodule Mix.Tasks.Try do
   use Mix.Task
 
   @impl Mix.Task
-  def run([module]) do
-    module = String.to_existing_atom(module)
-    Mix.shell().info(module.test())
-  end
+  def run(["Elixir." <> _ = module]), do: module |> String.to_existing_atom() |> execute
+  def run([module]), do: ("Elixir." <> module) |> String.to_existing_atom() |> execute
+  def run(_), do: raise("Invalid args, pass the module with the AOC behaviour implemented")
+
+  defp execute(module), do: Mix.shell().info(module.test())
 end
