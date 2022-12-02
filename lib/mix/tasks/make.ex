42a9b0e2b80d425c9b06b7e929f1b289bce7a9d4
Cesar Guzman <cesar.guzman@meetsam.io>
2022-12-02 15:01:34 +0100

    Makefile

---
+++ lib/mix/tasks/make.ex
@@ -0,0 +1,22 @@
+defmodule Mix.Tasks.Make do
+  use Mix.Task
+
+  @impl Mix.Task
+  def run(_args) do
+    app = Mix.Project.config()[:app]
+    app_string = app |> Atom.to_string()
+
+    app_module =
+      app_string |> String.split("_") |> Enum.map(&String.capitalize/1) |> Enum.join("")
+
+    makefile = """
+    test:
+      mix run -e "#{app_module}.test()"
+
+    solve:
+      mix run -e "#{app_module}.solve()"
+    """
+
+    File.write!("Makefile", makefile)
+  end
+end
